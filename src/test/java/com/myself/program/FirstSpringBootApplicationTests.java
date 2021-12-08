7f53d646fae26be69cc1994a9353e716f240d5e5
jie12012015 <jie_wang1201@163.com>
2021-12-08 20:35:08 +0800

    commit

---
+++ src/test/java/com/myself/program/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
